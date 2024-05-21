1393d8517c10cdbca85569e9a245ed5ac6547bce
ivannissimrch <ivannissimrch@gmail.com>
2024-05-20 21:22:39 -0400

    Implement basic game logic

---
+++ src/GameBoard.ts
@@ -0,0 +1,67 @@
+type Player = { name: string; wins: number; marker: string };
+
+export class GameBoard {
+  board: string[];
+  rows: number;
+  columns: number;
+  marker: string;
+  winner: string;
+
+  constructor() {
+    this.rows = 3;
+    this.columns = 3;
+    this.board = Array(this.rows * this.columns).fill("");
+    this.marker = "";
+    this.winner = "";
+  }
+
+  isValidMove(boardPositionValue: string) {
+    return boardPositionValue === "";
+  }
+
+  addMaker(player: Player, position: number) {
+    const validmove = this.isValidMove(this.board[position]);
+
+    if (validmove) {
+      this.marker = player.marker;
+      this.board[position] = this.marker;
+      //TODO Add a function for the aiplayer to add a marker
+    } else {
+      console.log("invalid move");
+      return;
+    }
+  }
+
+  resetBoard() {
+    this.board = Array(9).fill("");
+    this.winner = "";
+    this.marker = "";
+  }
+
+  checkWin() {
+    const winningCombinantions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 5, 6],
+    ];
+
+    for (const winningCombination of winningCombinantions) {
+      const [a, b, c] = winningCombination;
+      const { board } = this;
+
+      if (board[a] !== "" && board[a] === board[b] && board[a] === board[c]) {
+        this.winner = board[a];
+        return `winner is ${this.winner}`;
+      }
+      const isBoardFull = this.board.every((cell) => cell !== "");
+      if (isBoardFull) {
+        return "Draw";
+      }
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
 import "./style.css";
+import { GameBoard } from "./GameBoard";
+import { Player } from "./Player";
+
+const newGameBoard = new GameBoard();
+const player1 = new Player("player1");
+const player2 = new Player("aiPlayer");
+
+newGameBoard.addMaker(player1, 2);
+newGameBoard.addMaker(player2, 1);
+newGameBoard.addMaker(player1, 5);
+
+newGameBoard.addMaker(player2, 3);
+newGameBoard.addMaker(player1, 8);
+// newGameBoard.addMaker(player2, 5);
+// newGameBoard.addMaker(player1, 6);
+// newGameBoard.addMaker(player2, 7);
+// newGameBoard.addMaker(player1, 0);
+
+const win = newGameBoard.checkWin();
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>   
-  Tic Tac Toe Game
+  ${newGameBoard.board}
+  <h2>${win}</h2> 
   </div>
 `;
